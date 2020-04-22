deb8b8dd0ccc436863a29d7882d8855184e12809
Marin Bibot <Marin.bibot@hotmail.be>
2020-04-22 19:07:26 +0200

    fix mp4 export

--- app/javascript/src/containers/menu/animation.js
+++ app/javascript/src/containers/menu/animation.js
@@ -51,15 +51,14 @@ const Animation = (props) => {
     setExporting(true)
     setTimeout(() => {setExporting(false)}, frames.length * 1500)
   }
-
   return(
     <div className="submenu">
       <div className="submenu-header">
         Animation
       </div>
       <div className="submenu-actions">
-        <input type="number" className="submenu-el" style={{fontSize: '12px', width: '50px', textAlign: 'center'}} onChange={(e) => setFps(e.target.value)} defaultValue={fps}/>
-        <a href={`/animations/${animationId}/export?fps=${fps}`} onPointerDown={exportStarted} className={canExport() ? "submenu-el" : "submenu-el disabled"} download>
+        <input type="number" className="submenu-el" style={{fontSize: '12px', width: '50px', textAlign: 'center'}} onChange={(e) => setFps(e.target.value)} defaultValue={fps} />
+        <a href={`/animations/${animationId}/export?fps=${fps}`} download onClick={() => exportStarted()} onTouch={() => exportStarted()} className={canExport() ? "submenu-el" : "submenu-el disabled"} >
           <i className="fas fa-download submenu-el"></i>
         </a>
         <i className={playing ? "fas fa-pause-circle submenu-el" : "fas fa-play-circle submenu-el"} style={{ color: '#FF9B71' }} onPointerDown={toggleAnimation}></i>
